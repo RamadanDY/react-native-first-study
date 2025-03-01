44f106f88d0ef7a80ce14784cf40c2264f3405b5
RamDev <daudalaramadan2@gmail.com>
2025-03-01 11:31:18 +0000

    Refactor index component: update to functional component, add image and styles

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,33 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 import React from "react";
 
-const index = () => {
+const Index = () => {
   return (
-    <View>
-      <Text>Hello world!!!DD</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello world!!!</Text>
+      <Image
+        source={require("../images/passport.jpeg")} // Use require() for local images
+        style={styles.image} // Add styles for the image
+      />
     </View>
   );
 };
 
-export default index;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 50,
+    color: "blue",
+  },
+  image: {
+    width: 200,
+    height: 200,
+    borderRadius: 100 / 2,
+  },
+});
+
+export default Index;
